c51d53eceaa9d55dcc4235ad34664d50d4274967
Vlins <vlinsufba@gmail.com>
2016-01-02 20:33:27 -0300

    Refactored classes

--- pos_extractor.py
+++ pos_extractor.py
@@ -11,6 +11,7 @@ import codecs
 
 from nltk.corpus import treebank
 
+
 path = 'gesteira_corpus/'
 #path = '../dataset/test/'
 
@@ -21,18 +22,28 @@ def generate_bases():
             file_path = subdir + os.path.sep + file
             text = codecs.open(file_path, 'r',encoding='utf-8', errors='ignore' )
             lowers = text.read().lower()
+            tokens = " "
+            classification = ""
             tokenizer = RegexpTokenizer(r'\w+') 
             raw_tokens = tokenizer.tokenize(lowers)
-            tokens = " "
             for word in raw_tokens:
-                tokens += str(word) + " "
-            classification = tokens.split(' ', 1)[0]
+                if (tokens == " " and classification == "") or word == "cfb":
+                    classification += word
+                else:
+                    tokens += str(word) + " "
+            new_tokens = tokenizer.tokenize(tokens)
+#            classification = tokens.partition(' ')[0]
+#            tokens.replace("cfb", "", 1)
 #            filtered_tokens = [w for w in tokens if not w in stopwords.words('english')]
-            tagged = nltk.pos_tag(tokens)
+            tagged = nltk.tag.pos_tag(new_tokens)
             selected_tokens = [word for word,pos in tagged if pos =='JJ' or pos =='RB' or pos =='CC' and word != "and" ]
+            final_tokens = " "
+            for word in selected_tokens:
+                final_tokens += word +" "
             filename = str(counter) + "_" + classification
             counter += 1
-            open("output/file".replace('file',filename), "w").write(str(selected_tokens))
+            open("output/file".replace('file',filename), "w").write(str(final_tokens))
+
 
 def main():
     generate_bases()
--- preprocesssing.sh
+++ preprocesssing.sh
@@ -12,8 +12,10 @@ cd pretext
 perl Start.pl
 cd ..
 
-echo -e "@relation sentiment \n@classification {\"pos\", \"neg\"}\n"  >> $filename 
-tail -n +2 pretext/discover/discover.names  |  sed 's/"//g' | sed 's/:[^:]*$/ REAL/g'  | sed '$ s/.$//'   >> $filename
+echo -e "@relation sentiment \n"  >> $filename 
+echo -e "@attribute classification {\"cfb\", \"noncfb\"}"  >> $filename 
+#echo -e "@relation sentiment \n@classification {\"pos\", \"neg\"}\n"  >> $filename 
+tail -n +2 pretext/discover/discover.names  |  sed 's/"//g' | sed 's/^/@attribute /g' | sed 's/:[^:]*$/ REAL/g'  >> $filename
 echo -e "\n@data\n" >> $filename
 sed -i '2d' $filename
 sed s/...\output_Maid\.[0-9]*_//g pretext/discover/discover.data | sed s/,output_Maid//g >> $filename
