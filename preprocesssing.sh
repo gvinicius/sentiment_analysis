62f47c1ee749d24d0e975c1627d92c8dc28fa829
Vlins <vlinsufba@gmail.com>
2016-01-04 04:31:01 -0300

    Adapting the scripts to a non-git external directory for datasets

--- preprocesssing.sh
+++ preprocesssing.sh
@@ -5,19 +5,20 @@ filename="discover2.arff"
 
 : << END
 rm -Rf output/* output_Maid/*
-rm -Rf $filename
 
 python pos_extractor.py
-END
 cd pretext
 perl Start.pl
 cd ..
 
+END
+rm -Rf $filename
 echo -e "@relation sentiment \n"  >> $filename 
 echo -e "@attribute classification {\"neg\", \"pos\"}"  >> $filename 
 #echo -e "@relation sentiment \n@classification {\"pos\", \"neg\"}\n"  >> $filename 
 tail -n +2 pretext/discover/discover.names  |  sed 's/"//g' | sed 's/^/@attribute /g' | sed 's/:[^:]*$/ REAL/g'  >> $filename
 echo -e "\n@data\n" >> $filename
-sed -i '2d' $filename
-sed s/...\output_Maid\.[0-9]*_//g pretext/discover/discover.data | sed s/,output_Maid//g >> $filename
+#sed -i '2d' $filename
+sed s/^.*output_Maid\.[0-9]*_/\"/g pretext/discover/discover.data | sed s/,\\.\\.\\/dataset\\/output_Maid//g  >> $filename 
+#
 
