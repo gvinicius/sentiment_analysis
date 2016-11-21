44316fc49fdf6622e62ef0224d9eec028db8444b
gvinicius <gvinicius@ufba.br>
2016-11-21 05:35:20 -0300

    SVM working well

--- pos_extractor.py
+++ pos_extractor.py
@@ -10,24 +10,23 @@ import time
 import os
 
 import codecs
-
+import csv
 from nltk.corpus import treebank
 import numpy
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
-from pandas import DataFrame
+from sklearn.cross_validation import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
 # -*- coding: latin-1 -*-
-
-#path = 'gesteira_corpus/'
-path = '../dataset/Base-Canada-{0}'.format(sys.argv[1])
-destination_path = "../dataset/output-g-{0}".format(sys.argv[1])
-destination_path_file = destination_path + '/file'
-
-def generate_bases():
-#    with open('../dataset/economics/nuclear.csv', 'r', encoding="latin1") as csvfile:
-#    with open('../dataset/economics/nuclear.csv', 'r', encoding="latin1") as csvfile:
-
+def train_svm(X, y):
+    """
+    Create and train the Support Vector Machine.
+    """
+    svm = SVC(C=1000000.0, gamma=0.0, kernel='rbf')
+    svm.fit(X, y)
+    return svm
 
 def main():
     print ("Selected only words of desired POS.")
@@ -47,9 +46,18 @@ def main():
             final_tokens = ""
             for word in selected_tokens:
                 final_tokens += word + " " 
+            #final_tokens = tokenizer.tokenize(final_tokens)
             rows.append((final_tokens, classification))
+    rows_text = [r[0] for r in rows]
+    rows_label = [r[1] for r in rows]
+    #print(rows_text) 
     vectorizer = TfidfVectorizer(min_df=1)
-    X = vectorizer.fit_transform(rows[0])
+    X = vectorizer.fit_transform(rows_text)
+    X_train, X_test, y_train, y_test = train_test_split(X, rows_label, test_size=0.2, random_state=42)
+    svm = train_svm(X_train, y_train)
+    pred = svm.predict(X_test)
+    print(confusion_matrix(pred, y_test))
+    print(svm.score(X_test, y_test))
 #    run_classification = 'bash preprocesssing.sh ' + sys.argv[1]
 #    os.system(run_classification)
 if __name__ == "__main__":
