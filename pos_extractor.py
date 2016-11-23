9e8c6c5be8c1bf05d97e1ad6fb49f6bc38482cfd
gvinicius <gvinicius@ufba.br>
2016-11-23 01:54:52 -0300

    Modularizing result generation

--- pos_extractor.py
+++ pos_extractor.py
@@ -3,13 +3,13 @@ from nltk.corpus import wordnet
 from nltk.corpus import stopwords
 from nltk.corpus import treebank
 from nltk.tokenize import RegexpTokenizer
+from sklearn import tree
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cross_validation import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
-from sklearn import tree
 # -*- coding: latin-1 -*-
 
 def train_svm(X, y):
@@ -20,29 +20,29 @@ def train_svm(X, y):
     svm.fit(X, y)
     return svm
 
-def treat_csv_dataset(dataset, preprocessing_param):
-    classes_counter = collections.Counter()
+def treat_csv_dataset(dataset, text_field, category_field, preprocessing_param):
+    categories_counter = collections.Counter()
     with open(dataset, 'r', encoding="latin1") as csvfile:
         rows = []
         csv_reader = csv.reader(csvfile, quotechar='\"')
         reader = csv.DictReader(csvfile)
         for row in reader:
-            text = row['tweet_text']
-            classification = str(row['sentiment']).replace(" ","_").replace("/","").replace("'","")
-            classes_counter[classification] += 1
+            text = row[text_field]
+            category = str(row[category_field]).replace(" ","_").replace("/","").replace("'","")
+            categories_counter[category] += 1
             tokenizer = RegexpTokenizer(r'\w+') 
             raw_tokens = tokenizer.tokenize(text)
             tagged = nltk.tag.pos_tag(raw_tokens)
             if preprocessing_param == 'tags':
                 selected_tokens = [word for word,pos in tagged if pos in ['JJR','JJS','NNS','NNP'] ]
-            else:
+            elif preprocessing_param == 'no tags':
                 selected_tokens = raw_tokens
             final_tokens = ""
             for word in selected_tokens:
                 final_tokens += word + " " 
             #final_tokens = tokenizer.tokenize(final_tokens)
-            rows.append((final_tokens, classification))
-        return rows, classes_counter
+            rows.append((final_tokens, category))
+        return rows, categories_counter
 
 def generate_matrix(corpus):
     data = [c[0] for c in corpus]
@@ -66,16 +66,27 @@ def classify_by_algorithm(classifier_name, X_test, y_test):
     print(confusion_matrix(pred, y_test))
     print(classifier.score(X_test, y_test))
 
-def main():
-    print ("Automated classfication of sentiment analysis datasets.")
-    rows, classes_counter = treat_csv_dataset(sys.argv[1], sys.argv[2])
+def analyze(dataset, text_field, category_field, pos_condition):
+    rows, classes_counter = treat_csv_dataset(dataset, text_field, category_field, pos_condition)
     majoritary_class = classes_counter.most_common()[0][1]/(sum(classes_counter.values()))
     rows_text, rows_label = generate_matrix(rows)
     X = vectorize_data(rows_text)
-    X_train, X_test, y_train, y_test = train_test_split(X, rows_label, test_size=0.2, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, rows_label, test_size=0.1, random_state=0)
+    print(pos_condition)
     print(majoritary_class)
     classify_by_algorithm('SVM', X_test, y_test)
     classify_by_algorithm('NBM', X_test, y_test)
     classify_by_algorithm('C4.5', X_test, y_test)
+
+def main():
+    print ("Automated classfication of datasets for sentiment analysis.")
+    expected_args_quantity = 4
+    if len(sys.argv) != expected_args_quantity:
+        print('Wrong arguments numbers')
+        quit()
+    else:
+        dataset, text_field, category_field = sys.argv[1], sys.argv[2], sys.argv[3]
+        analyze(dataset, text_field, category_field, 'tags')
+        analyze(dataset, text_field, category_field, 'no tags')
 if __name__ == "__main__":
     main()
