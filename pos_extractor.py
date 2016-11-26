1e1e3813351a22e3fff36539d98e4f6164b2e0f4
gvinicius <gvinicius@ufba.br>
2016-11-26 20:32:42 -0300

    With 1-gram, bigram and 3-grams

--- pos_extractor.py
+++ pos_extractor.py
@@ -12,7 +12,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.multiclass import OneVsRestClassifier
 # -*- coding: latin-1 -*-
 
 def train_svm(x_data, y_data):
@@ -48,7 +49,7 @@ def generate_matrix(corpus):
 
 def vectorize_data(texts):
     """This function vectorizes text to matrices."""
-    vectorizer = TfidfVectorizer()
+    vectorizer = TfidfVectorizer(ngram_range=(1, 3))
     return vectorizer.fit_transform(texts)
 
 def classify_by_algorithm(classifier_name, x_situation, y_situation):
@@ -63,8 +64,11 @@ def classify_by_algorithm(classifier_name, x_situation, y_situation):
     print(classifier_name)
     print(confusion_matrix(prediction, y_situation))
     print(classification_report(prediction, y_situation))
-    print(classifier.score(x_situation, y_situation))
-    #print(cross_val_score(classifier, x_situation, y_situation, cv=10))
+    print("Score:{0}".format(classifier.score(x_situation, y_situation)))
+    kfold = KFold(n_splits=10)
+    print("Acc.:{0}".format(cross_val_score(classifier, x_situation, y_situation, cv=kfold).mean()))
+    print("Std.:{0}".format(cross_val_score(classifier, x_situation, y_situation, cv=kfold).std()))
+    return classifier
 
 def main():
     """Main funcation of the application."""
@@ -78,10 +82,11 @@ def main():
         majoritary_class = classes_counter.most_common()[0][1]/(sum(classes_counter.values()))
         rows_text, rows_label = generate_matrix(rows)
         x_raw = vectorize_data(rows_text)
-        x_train, x_test, y_train, y_test = train_test_split(x_raw, rows_label, test_size=0.1)
+        x_train, x_test, y_train, y_test = train_test_split(x_raw, rows_label, test_size=0.1, random_state=0)
         print("Majoritary class: {0}".format(majoritary_class))
         classifiers = ['SVM', 'NBM', 'C4.5']
+        predictions = []
         for classifier in classifiers:
-            classify_by_algorithm(classifier, x_test, y_test)
+            predictions.append(classify_by_algorithm(classifier, x_test, y_test))
 if __name__ == "__main__":
     main()
