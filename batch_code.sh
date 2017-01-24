06df79ed0eeebfc6ea3b82cfe830fcc6ba1e68b2
gvinicius <gvinicius@ufba.br>
2017-01-24 01:03:12 -0300

    Added a batch code for pca runs

--- batch_code.sh
+++ batch_code.sh
@@ -1,11 +1,12 @@
-python3 pos_extractor.py ../dataset/economics/1377191648_sentiment_nuclear_power.csv tweet_text sentiment
-python3 pos_extractor.py ../dataset/economics/1377884570_tweet_global_warming.csv tweet existence
-python3 pos_extractor.py ../dataset/economics/1384367161_claritin_october_twitter_side_effects-1.csv content sentiment 
-python3 pos_extractor.py ../dataset/economics/Airline-Sentiment-2-w-AA.csv text airline_sentiment 
-python3 pos_extractor.py ../dataset/economics/Apple-Twitter-Sentiment-DFE.csv text sentiment
-python3 pos_extractor.py ../dataset/economics/Coachella-2015-2-DFE.csv text coachella_sentiment
-python3 pos_extractor.py ../dataset/economics/Deflategate-DFE.csv text deflate_sentiment
-python3 pos_extractor.py ../dataset/economics/GOP_REL_ONLY.csv text sentiment
-python3 pos_extractor.py ../dataset/economics/New-years-resolutions-DFE.csv text resolution_topics
-python3 pos_extractor.py ../dataset/economics/Political-media-DFE.csv message bias
-python3 pos_extractor.py ../dataset/economics/progressive-tweet-sentiment.csv tweet_text sentiment
+echo '' > pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/1377191648_sentiment_nuclear_power.csv tweet_text sentiment >> pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/1377884570_tweet_global_warming.csv tweet existence >> pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/1384367161_claritin_october_twitter_side_effects-1.csv content sentiment  >> pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/Airline-Sentiment-2-w-AA.csv text airline_sentiment  >> pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/Apple-Twitter-Sentiment-DFE.csv text sentiment >> pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/Coachella-2015-2-DFE.csv text coachella_sentiment >> pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/Deflategate-DFE.csv text deflate_sentiment >> pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/GOP_REL_ONLY.csv text sentiment >> pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/New-years-resolutions-DFE.csv text resolution_topics >> pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/Political-media-DFE.csv message bias >> pos_res.txt
+python3 lda_pca_sent.py ../dataset/economics/progressive-tweet-sentiment.csv tweet_text sentiment >> pos_res.txt
--- lda_pca_sent.py
+++ lda_pca_sent.py
@@ -54,12 +54,12 @@ def vectorize_data(texts, condition):
     vectorizer = TfidfVectorizer()
     transformation = vectorizer.fit_transform(texts)
     if condition == 'pca':
-        pca = PCA()
+        pca = TruncatedSVD(algorithm='arpack', random_state=0)
         transformation = pca.fit_transform(transformation)
         # r = robjects.r
         # pca = r.princomp(transformation)
         # transformation =  pca.rotation
-    dimensionality_notion = len(transformation.toarray()[0])
+    dimensionality_notion = 1 # len(transformation.toarray()[0])
     return transformation, dimensionality_notion
 
 def classify_by_algorithm(classifier_name, x_test, y_test, kfold):
@@ -88,8 +88,8 @@ def main():
         dataset, text_column, category_column = sys.argv[1], sys.argv[2], sys.argv[3]
         print(dataset)
         matrix_conditions = ['tdidf', 'pca']
-        classifiers = ['SVM', 'NBM', 'CART']
-        result_labels = ['SVM', 'NBM', 'CART']
+        classifiers = ['SVM', 'CART']
+        result_labels = ['SVM', 'CART']
         result_labels.append('dim')
         runs_set = []
         results_set = []
@@ -123,6 +123,7 @@ def main():
                 csv_results.append(dim)
                 csvwriter.writerow(csv_results)
         """ Statistical test to check tagging technique impact. """
+        """
         elements_notag = [results_set[0], results_set[1], results_set[2]]
         elements_tag = [results_set[3], results_set[4], results_set[5]]
         h_statistic, p_value = stats.mannwhitneyu(elements_notag, elements_tag)
@@ -131,5 +132,6 @@ def main():
         with open('results.csv', 'a') as csvfile:
             csvwriter = csv.writer(csvfile, delimiter=',')
             csvwriter.writerow([round(p_value,2)])
+        """
 if __name__ == "__main__":
     main()
